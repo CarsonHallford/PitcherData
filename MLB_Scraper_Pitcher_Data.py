09275205227be608675ca318cccc7e87b0ce1471
CarsonHallford <carson.hallford@gmail.com>
2025-03-03 14:58:25 -0600

    fifth commit

--- MLB_Scraper_Pitcher_Data.py
+++ MLB_Scraper_Pitcher_Data.py
@@ -1,6 +1,5 @@
 # MLB Scraper Pitcher Data
 import pandas as pd
-import pybaseball as pyb
 import numpy as np
 from api_scraper import MLB_Scrape
 
@@ -27,98 +26,7 @@ pandas_df = data_df.to_pandas()
 
 df_pyb = data_df.to_pandas()
 df_pyb["PitchesThrown"] = 1
-hit_descriptions = {
-    "field_out": 0,
-    "nan": 0,
-    "strikeout": 0,
-    "double": 1,
-    "strikeout_double_play": 0,
-    "single": 1,
-    "force_out": 0,
-    "hit_by_pitch": 0,
-    "grounded_into_double_play": 0,
-    "home_run": 1,
-    "walk": 0,
-    "caught_stealing_2b": 0,
-    "sac_bunt": 0,
-    "triple": 1,
-    "sac_fly": 0,
-    "field_error": 0,
-    "double_play": 0,
-    "catcher_interf": 0,
-    "fielders_choice_out": 0,
-    "fielders_choice": 0,
-    "pickoff_1,b": 0,
-    "other_out": 0,
-    "caught_stealing_home": 0,
-    "pickoff_caught_stealing_2b": 0,
-    "caught_stealing_3b": 0,
-    "sac_fly_double_play": 0,
-    "pickoff_caught_stealing_home": 0,
-    "pickoff_2b": 0,
-    "run": 0,
-    "triple_play": 0,
-    "batter_interference": 0,
-    "pickoff_3b": 0,
-    "sac_bunt_double_play": 0,
-    "pickoff_caught_stealing_3b": 0,
-}
 
-
-ab_flag_dict = {
-    "field_out": 1,
-    "nan": 0,
-    "strikeout": 1,
-    "double": 1,
-    "strikeout_double_play": 1,
-    "single": 1,
-    "force_out": 1,
-    "hit_by_pitch": 0,
-    "grounded_into_double_play": 1,
-    "home_run": 1,
-    "walk": 0,
-    "caught_stealing_2b": 0,
-    "sac_bunt": 0,
-    "triple": 1,
-    "sac_fly": 0,
-    "field_error": 1,
-    "double_play": 1,
-    "catcher_interf": 0,
-    "fielders_choice_out": 1,
-    "fielders_choice": 1,
-    "pickoff_1b": 0,
-    "other_out": 0,
-    "caught_stealing_home": 0,
-    "pickoff_caught_stealing_2b": 0,
-    "caught_stealing_3b": 0,
-    "sac_fly_double_play": 1,
-    "pickoff_caught_stealing_home": 0,
-    "pickoff_2b": 0,
-    "run": 0,
-    "triple_play": 1,
-    "batter_interference": 1,
-    "pickoff_3b": 0,
-    "sac_bunt_double_play": 1,
-    "pickoff_caught_stealing_3b": 0,
-}
-
-swing_code = {
-    "ball": 0,
-    "foul_tip": 1,
-    "called_strike": 0,
-    "swinging_strike": 1,
-    "pitchout": 0,
-    "bunt_foul_tip": 1,
-    "foul": 1,
-    "hit_into_play_no_out": 1,
-    "hit_into_play": 1,
-    "hit_into_play_score": 1,
-    "missed_bunt": 1,
-    "hit_by_pitch": 0,
-    "blocked_ball": 0,
-    "swinging_strike_blocked": 1,
-    "foul_bunt": 1,
-}
 
 is_ball = [11, 12, 13, 14]
 
